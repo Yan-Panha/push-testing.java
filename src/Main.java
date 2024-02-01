450112b41f83bd832c6c40e46dc869b53766cd25
yan panha <yanpanha497@gmail.com>
2024-02-01 08:57:14 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame f=new Frame("Testing");
+        Button btn = new Button("Testing Click");
+        TextField tf = new TextField( 20);
+        f.add(btn);
+        f.add(tf);
+        f.setLayout(new FlowLayout());
+        f.setSize(500,500);
+        f.setVisible(true);
+
+    }
+}
